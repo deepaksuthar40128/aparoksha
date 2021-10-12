ea060470c26911e3af71328e038b20bb05ab176b
Amusa Abayomi <amusaabayomipaul99@gmail.com>
2021-10-12 08:21:39 +0100

    tutorial files of how to integrate atropos js

---
+++ index.js
@@ -0,0 +1,58 @@
+// atroposJS parameters
+// rotateLock: true,
+// rotateXMax: 15,
+// rotateYMax: 15,
+// shadow: true,
+// shadowOffset: 10,
+// shadowScale: 0.8,
+// highlight: false,
+// durationEnter: 600,
+// activeOffset: 60
+
+const myAtropos = Atropos({
+    el: '.my-atropos',
+    // rest of parameters
+    rotateLock: true,
+
+    // rotateXMax: 75,
+    // rotateYMax: 75,
+
+    shadow: true,
+    shadowOffset: 50,
+    // shadowScale: 5,
+    // highlight: false,
+    // durationEnter: 1000,
+    // activeOffset: 560
+});
+
+const mySecondAtropos = Atropos({
+    el: '.my-second-atropos',
+    // rest of parameters
+    rotateLock: true,
+
+    // rotateXMax: 75,
+    // rotateYMax: 75,
+
+    shadow: true,
+    shadowOffset: 50,
+    // shadowScale: 5,
+    // highlight: false,
+    // durationEnter: 1000,
+    // activeOffset: 560
+});
+
+const myThirdAtropos = Atropos({
+    el: '.my-third-atropos',
+    // rest of parameters
+    rotateLock: true,
+
+    // rotateXMax: 75,
+    // rotateYMax: 75,
+
+    shadow: true,
+    shadowOffset: 50,
+    // shadowScale: 5,
+    // highlight: false,
+    // durationEnter: 1000,
+    // activeOffset: 560
+});
