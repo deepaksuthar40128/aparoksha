dd7f00f7d756bb35b6779a14fda72e8b4d6e562e
deepak suthar <deepaksuthar40128@gmail.com>
2023-04-17 17:50:59 +0530

    intials

--- app.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener('scroll', e => {
-    document.documentElement.style.setProperty('--scrollTop', `${this.scrollY}px`) // Update method
-})
-gsap.registerPlugin(ScrollTrigger, ScrollSmoother)
-ScrollSmoother.create({
-    wrapper: '.wrapper',
-    content: '.content'
-})
